4f2cbd0bebbdbfd5c4114d8b9c45c11f709b46f2
ThanhNhaApac <142214778+thanhnhaapac@users.noreply.github.com>
2024-04-15 11:00:07 +0700

    Add files via upload

---
+++ src/constant.ts
@@ -0,0 +1,24 @@
+export const FULLNODE_URL = "https://fullnode.devnet.sui.io";
+
+export const CLIENT_ID =
+  "573120070871-0k7ga6ns79ie0jpg1ei6ip5vje2ostt6.apps.googleusercontent.com";
+
+export const REDIRECT_URI = "https://sui-zklogin.vercel.app/";
+
+export const SUI_DEVNET_FAUCET = 'https://faucet.devnet.sui.io/gas'
+
+export const SUI_PROVER_DEV_ENDPOINT = 'https://prover-dev.mystenlabs.com/v1'
+
+export const KEY_PAIR_SESSION_STORAGE_KEY = "demo_ephemeral_key_pair";
+
+export const USER_SALT_LOCAL_STORAGE_KEY = "demo_user_salt_key_pair";
+
+export const RANDOMNESS_SESSION_STORAGE_KEY = "demo_randomness_key_pair";
+
+export const MAX_EPOCH_LOCAL_STORAGE_KEY = "demo_max_epoch_key_pair";
+
+
+export const STEPS_LABELS_TRANS_KEY = ['16e758e8', '9b8b5398', '8adf5b45', '8b72e7cd', '66f6b490', 'af802c7a', 'c649dd70',]
+
+
+export const STEPS_DESC = ['ephemeralKeyPair', '47b83f4e', 'fb399be8', '0a710e64', '32255d31', '8f2433d9']
